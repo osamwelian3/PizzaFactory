5093233e4a313d144075831a275ac27c1103e3de
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-03-20 15:48:18 +0300

    script: It will define all the constructors for objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
 // declare variables
 const deliveryFee = 200;
 var size, crust, topping, quantity, delivery, dlocation;
+
+// Constructors
+function Pizza (size, price, crust, topping, quantity, customer) {
+    this.size = size;
+    this.price = price;
+    this.crust = crust;
+    this.topping = topping;
+    this.quantity = quantity;
+    this.customer = customer;
+}
+
+function customer(name, customerLocation) {
+    this.name = name;
+    this.customerLocation = customerLocation;
+}
+
+function Crust (crustName, crustPrice) {
+    this.crustName = crustName;
+    this.crustPrice = crustPrice;
+}
+
+function Topping (tName, tPrice, basePrice) {
+    this.tName = tName;
+    this.basePrice = basePrice;
+    this.tPrice = tPrice;
+}
